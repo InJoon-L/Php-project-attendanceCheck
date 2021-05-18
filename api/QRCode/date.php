b16df19f4f48e8bd2e65f93ee56b568e4208d699
InJoon-L <dlswns619@naver.com>
2021-05-18 16:23:27 +0900

    UPDATE::QRCode logic

--- api/QRCode/date.php
+++ api/QRCode/date.php
@@ -2,6 +2,14 @@
     date_default_timezone_set('Asia/Seoul');
     $date = date("Y-m-d");
     $time = date("H:i:s");
-    echo $date . "\n";
-    echo $time;
+    $startAMTime = "07:00:00";
+    $endAMTime = "09:00:00";
+    $startPMTime = "18:50:00";
+    $endPMTime = "19:50:00";
+
+    if ($startAMTime < $time) {
+        echo "good";
+    }
+    echo $startAMTime;
+    echo $startPMTime;
 ?>
--- api/QRCode/qrCheck.php
+++ api/QRCode/qrCheck.php
@@ -1,29 +1,49 @@
 <?php
-// require_once('../connDB.php');
-// require_once('../res.php');
-
 // 입력 값을 JSON으로 decoding 실시 -> 객체 생성
 $req = json_decode(file_get_contents('php://input'));
 
 // DBMS QR코드 레코드 삽입
 function QRCodeInsertRecordsFromTable($argObj) {
     $dbConn = makeDBConnection();
-    $when = null;
     $date = date("Y-m-d");
     $time = date("H:i:s");
-    // 시간에 맞는 출석 알고리즘 필요
-    // if (true) {
-    //     $when = "am_attendance";
-    //     $status = "출";
-    // }
+    $status = null;
+    $startAMTime = "07:00:00";
+    $endAMTime = "09:00:00";
+    $endTime = "17:50:00";
 
-    $sql_stmt = "INSERT INTO Attendance_status (date, am_attendance, status, m_id)
-    VALUES(\"{$argObj->date}\", \"{$argObj->attendance}\", \"{$argObj->status}\", {$argObj->id})";
-    
-    if ($result = $dbConn->query($sql_stmt)) {
-        $dbConn->close();
-        return $argObj;
+    // 시간에 맞는 출석 알고리즘 필요
+    // 오전 9시전까지 출석, 오전 9시 ~ 오후 5시 50분 지각, 오후 6시 50분 ~ 오후 7시 야자 출석, 오후 7시 50분 이후 퇴근
+    // 오전 9시(혹은 오후 6시)가 지나고 QR코드를 한번도 입력이 안된 사람들을 찾아서 결석으로 지정 문제는 서버에 요청을
+    // 어떻게 해야 될까
+    // 그 이후 status를 조정 조퇴의 경우 무조건 수동입력으로만 해결 할 지 QR코드로 해결이 가능한 지
+    // 결석의 대한 문제만 해결하면 끝
+    if ($time > $startAMTime && $time < $endAMTime) {
+        $status = "출석";
+    } else if ($time > $endAMTime && $time < $endTime) {
+        $status = "지각";
     }
+    
+    if ($argObj->attendance === "am_attendance") {
+        $sql_stmt = "INSERT INTO Attendance_status (date, {$argObj->attendance}, status, m_id, m_name)
+        VALUES(\"$date\", \"$time\", \"$status\", {$argObj->id}, \"{$argObj->name}\")";
+    
+        if ($result = $dbConn->query($sql_stmt)) {
+            $dbConn->close();
+            
+            return $argObj;
+        }
+    } else {
+        $sql_stmt = "UPDATE Attendance_status SET {$argObj->attendance} =
+        \"$time\" WHERE m_id = {$argObj->id}";
+
+        if ($result = $dbConn->query($sql_stmt)) {
+            $dbConn-> close();
+
+            return $argObj;
+        }
+    }
+    
 
     $dbConn->close();
     return null;
