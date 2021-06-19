5e7f6f575dd68092b10ff1974f8206cdab084394
InJoon-L <dlswns619@naver.com>
2021-06-19 13:25:34 +0900

    자동 결석 구현

--- api/QRCode/qrCheck.php
+++ api/QRCode/qrCheck.php
@@ -2,8 +2,39 @@
 // 입력 값을 JSON으로 decoding 실시 -> 객체 생성
 $req = json_decode(file_get_contents('php://input'));
 
+// 결석 상태 체크
+function UserAbsentStatus($argObj) {
+    $dbConn = makeDBConnection();
+    $dateTarget = date("Y-m-d", strtotime("-4 months")); // 현재 날짜에서 4개월을 뺀 날짜
+    $dateNow = date("Y-m-d", strtotime('-1 days')); // 현재 날짜 -1일
+
+    // 현재 날짜에서 4개월은 뺀 날로부터 현재 날짜 -1일에 대한 데이터 중 가장 최근 날짜 조회
+    $sql_stmt = "SELECT date From Attendance_status where m_id = $argObj->id && $dateTarget < date <= $dateNow && status IN ('출석', '지각') order by date DESC";
+
+    if ($result = $dbConn->query($sql_stmt)) {
+        $dateResult = $result->fetch_assoc();
+
+        // $dateResult값을 넣으면 빈값이 나오기 때문에 문자열로 변환
+        $newDate = $dateResult['date']; // Select문 문자열로 변환
+
+ 
+        // 최근 출석한 날짜와 현재 -1일 날짜가 같다면 함수 종료
+        if (strtotime($newDate) == strtotime($dateNow)) return;
+
+        $newDate2 = date("Y-m-d", strtotime($newDate."+1day")); // 데이트에 +1일 더해서 변환
+
+        // SELECT한 날짜부터 현재 -1일까지 반복을 통하여 결석값을 넣어준다.
+        for ($i = $newDate2; $i <= $dateNow; $i++) {
+            // 출석현황 데이터에 날짜 + 결석 + 학번을 추가해준다.
+            $sql_stmt2 = "INSERT INTO Attendance_status (date, status, m_id, m_name) values ('$i', '결석', $argObj->id, '$argObj->name')";
+            $dbConn->query($sql_stmt2);
+        }
+    }
+}
+
 // DBMS QR코드 레코드 삽입
 function QRCodeInsertRecordsFromTable($argObj) {
+    UserAbsentStatus($argObj);
     $dbConn = makeDBConnection();
     $date = date("Y-m-d");
     $time = date("H:i:s");
